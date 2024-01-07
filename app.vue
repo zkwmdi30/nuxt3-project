f52d2539006903d1483eb85e6a22da9a7df250f3
이명필 <zkwmdi30@gmail.com>
2024-01-07 12:55:39 +0900

    최초 업로드

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+      <!-- 헤더 내용 -->
+    </header>
+    
+    <main>
+      <!-- 페이지가 여기에 렌더링 될 것입니다. -->
+      <Nuxt />
+    </main>
+
+    <footer>
+      <!-- 푸터 내용 -->
+    </footer>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,10 @@
+<template>
+    <section>
+      <p>This page will be displayed at the /about route.</p>
+    </section>
+</template>
+<script>
+    export default {
+        // about.vue 파일에서 다른 설정 옵션들도 포함시킬 수 있습니다.
+    }
+</script>
